4c3417117a7b6da359f04d05fa1d2e9e6aa120de
Dylan Ledbetter <dylan@ledbetter.fm>
2020-10-26 00:41:04 -0500

    remove plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,42 +32,5 @@ module.exports = {
         siteUrl: targetAddress.href.slice(0, -1),
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `LEDBETTER.FM`,
-        short_name: `LEDBETTER.FM`,
-        start_url: `/`,
-        display: `standalone`,
-        icon: `static/favicon.ico`,
-        icons: [
-          {
-            src: `static/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `static/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-          {
-            src: `static/favicon-16x16.png`,
-            sizes: `16x16`,
-            type: `image/png`,
-          },
-          {
-            src: `static/favicon-32x32.png`,
-            sizes: `32x32`,
-            type: `image/png`,
-          },
-          {
-            src: `static/apple-touch-icon.png`,
-            sizes: `180x180`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
   ],
 }
